27bd25c8b40071e09e57555c73f2e0b71ad3e935
vedikasaklani <ivedikasaklani@gmail.com>
2025-09-07 11:49:20 +0530

    Upload day2_email_checker.py

--- day2_email_checker.py
+++ day2_email_checker.py
@@ -1,9 +1,10 @@
 import re
 def email_checker():
     s=str(input("Enter email to check: "))
-    pattern=re.compile(r"^[^\._](?!.*\.\.)[a-zA-Z0-9_\.)+@[a-zA-Z0-9\-]+(\.[a-z]{2,})+$")
+    pattern=re.compile(r"^[^\._](?!.*\.\.)[a-zA-Z0-9_\.]+@[a-zA-Z0-9\-]+(\.[a-z]{2,})+$")
     if pattern.search(s):
         print("Valid email")
     else:
         print("Invalid email")
 email_checker()
+
